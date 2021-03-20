6fe4edae523bf52248804e323d991e02364f1d98
G-dH <georgdh@gmail.com>
2021-03-20 13:19:53 +0100

    update for GS 3.36

--- overlay-followfocus@amongpixels.com/extension.js
+++ overlay-followfocus@amongpixels.com/extension.js
@@ -1,12 +1,10 @@
 const Main = imports.ui.main;
 const Workspace = imports.ui.workspace;
-
 let overlayInjections, workspaceInjections;
 
 function resetState() {
-    overlayInjections = { };
-    workspaceInjections = { };
-    windowToFocus = null;
+    overlayInjections = {};
+    workspaceInjections = {};
 }
 
 function injectToFunction(parent, name, func) {
@@ -35,20 +33,19 @@ function enable() {
 
     resetState();
     
-    overlayInjections['_onEnter'] = injectToFunction(Workspace.WindowOverlay.prototype, '_onEnter', function () {
+    overlayInjections['_onShowChrome'] = injectToFunction(Workspace.WindowOverlay.prototype, '_onShowChrome', function () {
         this._windowClone.mouseOvered = true;
     });
     
-    overlayInjections['_onLeave'] = injectToFunction(Workspace.WindowOverlay.prototype, '_onLeave', function () {
+    overlayInjections['_onHideChrome'] = injectToFunction(Workspace.WindowOverlay.prototype, '_onHideChrome', function () {
         this._windowClone.mouseOvered = false;
     });
     
     workspaceInjections['zoomFromOverview'] = injectToFunction(Workspace.Workspace.prototype, 'zoomFromOverview', function () {
-        
         // See if any window was mouseovered at the time of going back to normal desktop
         for (let i = 0; i < this._windows.length; i++) {
             let clone = this._windows[i];
-            
+
             if ( (clone.mouseOvered !== undefined) && (clone.mouseOvered) ) {
                 // Activate that window
                 clone.metaWindow.activate(global.get_current_time());
@@ -56,20 +53,17 @@ function enable() {
         }
         
     });
-    
 }
 
 function disable() {
 
-    for (i in overlayInjections) {
+    for (let i in overlayInjections) {
         removeInjection(Workspace.WindowOverlay.prototype, overlayInjections, i);
         global.log("removing injection " + i);
     }
     
-    for (i in workspaceInjections) {
+    for (let i in workspaceInjections) {
         removeInjection(Workspace.Workspace.prototype, workspaceInjections, i);
     }
-    
     resetState();
-    
 }
